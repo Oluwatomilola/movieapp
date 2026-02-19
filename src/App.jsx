99b51f24e564cf396acac615d15bf8016c086275
Oluwatomilola <tommy4gold@gmail.com>
2026-02-19 22:33:18 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from "react";
-import reactLogo from './assets/react.svg';
-import viteLogo from './vite.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 const Card = ({title}, {ratings}, {review}) => {
+
+    const [hasLiked, setHasLiked] = useState(initialState: false);
+
+
   return (
-    <div>
+    <div className="card">
       <h2>{title}</h2>
+      <button onClick={() => setHasLiked(value: true)}>
+        Like
+      </button>
     </div>
   )
 }
 
 const App = () => {
+
+
   return (
     <div className="card-container">
       <Card title="Lion King" ratings={5} review={cool} actors={[{name: Simba}, {name: Hakuna}, {name:Matata},]} />
