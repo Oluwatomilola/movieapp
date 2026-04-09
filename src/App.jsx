af45ecc03c369380d0f079b0bf42a8db2f37661e
Oluwatomilola <tommy4gold@gmail.com>
2026-04-09 13:00:47 +0100

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import React from 'react';
-import { useState } from 'react';
-import './App.css';
-
-const Card = ({title}, {ratings}, {review}) => {
-
-    const [hasLiked, setHasLiked] = useState(initialState: false);
-
-
-  return (
-    <div className="card">
-      <h2>{title}</h2>
-      <button onClick={() => setHasLiked(value: true)}>
-        Like
-      </button>
-    </div>
-  )
-}
+import Search from './components/Search.jsx'
+import { useState } from 'react'
 
 const App = () => {
-
+  const [searchTerm, setSearchTerm] = useState('');
 
   return (
-    <div className="card-container">
-      <Card title="Lion King" ratings={5} review={cool} actors={[{name: Simba}, {name: Hakuna}, {name:Matata},]} />
-      <Card title="Star Wars" ratings={5} review={cool} actors={[{name: Simba}, {name: Hakuna}, {name:Matata},]} />
-      <Card title="Mufasa" ratings={5} review={cool} actors={[{name: Simba}, {name: Hakuna}, {name:Matata},]} />
-      <Card title="Black Panther" ratings={5} review={cool} actors={[{name: Simba}, {name: Hakuna}, {name:Matata},]} />
-    </div>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png"  alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span> you'll enjoy without the hassle</h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
+      </div>
+    </main>
   )
 }
-
-
 
 export default App
---
+++ src/components/Search.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="./search.svg" alt="Search" />
+        <input 
+            type="text"
+            className="w-full bg-transparent py-2 sm:pr-10 pl-10 text-base text-gray-200 placeholder-light-200 outline-hidden"
+            placeholder="Search through thousands of movies"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div> .  
+
+      </div>
+  )
+}
+
+export default Search
